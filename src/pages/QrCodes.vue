a33f656620ca27cbe82f6e8c25799241d70a29be
Max Moyano <Experimental.1432@gmail.com>
2017-12-05 12:34:22 -0300

    V 0.3.1

--- src/pages/QrCodes.vue
+++ src/pages/QrCodes.vue
@@ -3,9 +3,9 @@
     .column.is-4.is-mobile-12
       img(src="https://translatethis.github.io/Frutexsa/dist/ID1.jpg")
     .column.is-4.is-mobile-12
-      img(src="https://translatethis.github.io/Frutexsa/dist/ID2.jpg")
+      img(src="https://translatethis.github.io/Frutexsa/dist/ID2.png")
     .column.is-4.is-mobile-12
-      img(src="https://translatethis.github.io/Frutexsa/dist/ID3.jpg")
+      img(src="https://translatethis.github.io/Frutexsa/dist/ID3.png")
 </template>
 
 <script>
